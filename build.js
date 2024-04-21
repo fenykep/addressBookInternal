0bc78bcb7eafb34f1793f042e42ed46cd252a92b
fenykep <abel.fenykep@gmail.com>
2024-04-21 14:03:38 +0200

    sw to js build script

---
+++ build.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const pug = require('pug');
+const sass = require('sass');
+
+// Read the JSON data from the file
+const data = JSON.parse(fs.readFileSync('src/testData3.json', 'utf8'));
+
+// Render the index.pug file with the data
+const html = pug.renderFile('src/index.pug', { cities: data.cities });
+
+// Write the rendered HTML to the file
+fs.writeFileSync('public/index.html', html);
+
+// Render the style.scss file to style.css
+const result = sass.renderSync({
+  file: 'src/style.scss',
+  outputStyle: 'compressed'
+});
+
+// Write the rendered CSS to the file
+fs.writeFileSync('public/style.css', result.css);
